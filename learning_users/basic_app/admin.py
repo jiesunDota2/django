07436cb603c895252b1e1508c2bfbe94f1bd4390
jsun <jsun@anomali.com>
2020-03-23 01:54:34 -0700

    bug

--- learning_users/basic_app/admin.py
+++ learning_users/basic_app/admin.py
@@ -2,6 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import UserProfileInfo
+from .models import UserProfileInfo
 # Register your models here.
 admin.site.register(UserProfileInfo)
